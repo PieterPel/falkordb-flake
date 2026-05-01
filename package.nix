58e3a7b724317ee248958c19a0d057e3eea29007
PieterPel <25645555+PieterPel@users.noreply.github.com>
2026-05-01 08:06:18 +0000

    chore: update FalkorDB to v4.18.3

--- package.nix
+++ package.nix
@@ -1,20 +1,20 @@
 { lib, stdenv, fetchurl, autoPatchelfHook, openssl }:
 
 let
-  version = "4.18.2";
+  version = "4.18.3";
 
   sources = {
     "aarch64-darwin" = {
       url = "https://github.com/FalkorDB/FalkorDB/releases/download/v${version}/falkordb-macos-arm64v8.so";
-      hash = "sha256-I3MqiO6hhPxVq00B8cr0Hxjbyev7Qy1NThi82TYzqXs=";
+      hash = "sha256-U6qY5m3FLPTZVijRFEq08yM8rflR+vgedtWnxESDVBo=";
     };
     "x86_64-linux" = {
       url = "https://github.com/FalkorDB/FalkorDB/releases/download/v${version}/falkordb-x64.so";
-      hash = "sha256-TTR+nPuBh7FDNbXrLRSx48wSfkh/vD2dlPUtHUT+wk4=";
+      hash = "sha256-R4heLaeIw/uCK5vUwYKpaU1nKGp/2P4Ywz48Gg0FY2s=";
     };
     "aarch64-linux" = {
       url = "https://github.com/FalkorDB/FalkorDB/releases/download/v${version}/falkordb-arm64v8.so";
-      hash = "sha256-Abn5jBmNMpSy+4KxYBGnj93zWD2CwrBszyJe9YStZpU=";
+      hash = "sha256-Eacd7TDnpJLiwwWS/drr4RmJAiTeCdmpMdhqT1ZKNNY=";
     };
   };
 
